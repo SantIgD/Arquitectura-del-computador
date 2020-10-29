1afc7868a7cdcc2ddd99a655f7c03ca79b48c9a4
Santi D <demagistris.santiago@gmail.com>
2020-10-29 19:02:26 -0300

    [Entrega 8] [Ejercicio 5]  EPICO

--- Ejercicio 5/fact1.s
+++ Ejercicio 5/fact1.s
@@ -2,18 +2,22 @@
 .global fact1
 
 fact1:
-    xorq %rax,%rax    # Ponemos en 0 rax
-    incq %rax         # Ponemos en 1 rax 
+   cmpq $1,%rdi
+   jb caso_base   # rdi <= 1
+   jmp caso_recursivo
 
-comparar:
-    andl %edi,%edi    # Verificar que edi no almacene el valor 0
-    jz es_0           # Si es 0 hace un salto al retorno
-
-    mull %edi         # Multiplicamos el valor de edi por el valor de eax y se almacena en eax
-    decl %edi         # Decrementamos el valor de edi en 1
-    jmp comparar      # Saltamos a comparar
+caso_base:
+   xorq %rax ,%rax
+   incq %rax
+   jmp fin
    
-es_0:
-    ret               # Retornamos el valor en rax donde solo eax tiene informacion
+caso_recursivo:
+   pushq %rdi  
+   decq %rdi
+   call fact1
+   popq %rdi
+   mull %edi
 
-    
+fin:
+   ret
+
