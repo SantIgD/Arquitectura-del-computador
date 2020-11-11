f4c3331b4e642ad3acc811ee839e7a25c5d1f69a
Santi D <demagistris.santiago@gmail.com>
2020-11-11 19:01:02 -0300

    [Ejercicio 12] Terminado

--- Ejercicio 12/e12.c
+++ Ejercicio 12/e12.c
@@ -5,7 +5,7 @@ task t1, t2,t3, taskmain;
 
 void ft1(){
 	double d;
-    printf("[dir] t1: &d= 0x%lX\n sizeof double: %d\n",&d,sizeof(d));
+    printf("[dir] t1: &d= %p\n",&d);
 	for(d=-1;;d+=0.001) {
 		printf("t1: d=%f\n", d);
 		TRANSFER(t1,t2);
@@ -13,8 +13,8 @@ void ft1(){
 }
 
 void ft2(){
-	int i;
-    printf("[dir] t2: &i= 0x%lX\n  sizeof int: %d\n",&i,sizeof(i));
+	int i=10;
+    printf("[dir] t2: &i= %p\n",&i);
 	for(i=0;i<10000;i++) {
 		printf("t2: i=%d\n", i);
 		TRANSFER(t2,t3);
@@ -24,8 +24,8 @@ void ft2(){
 
 
 void ft3(void){
-  unsigned j;
-  printf("[dir] t3: &j= 0x%lX\n sizeof unsigned: %d\n",&j,sizeof(j));
+  unsigned j=10;
+  printf("[dir] t3: &j= %p\n",&j);
   scanf("%d",&j);
   for (j = 0; j < 5000; j++) {
     printf("t3: j=%u\n", j);
@@ -39,7 +39,6 @@ int main(){
 	stack(t1,ft1);
 	stack(t2,ft2);
     stack(t3,ft3);
-	printf("SizeofChar: %d\n",sizeof(char));
 	TRANSFER(taskmain,t1);
 	return 0;
 }
---
+++ Ejercicio 12/probando.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void f(int a, int b, int c){
+    int jeje=10;
+}
+
+
+int main(){
+    
+    f(1,2,3);
+
+    return 0;
+}
