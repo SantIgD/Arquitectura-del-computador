c6d6ec4873d3f45fa819d56d50a0c77615876427
Santi D <demagistris.santiago@gmail.com>
2020-11-13 17:10:01 -0300

    [Ejercicio 7-8] Hacer funciones correspondientes, generar arreglo, prints y comparar tiempos

---
+++ Ejercicio 7 y 8/ej7-8.s
@@ -0,0 +1,75 @@
+.text
+.global sum
+.global sum_simd
+
+sum:
+
+    #rdi *a
+    #rsi *b
+    #rdx len
+
+    xorq %rcx,%rcx
+    orl %edx,%ecx
+
+
+    
+    # 1 float => 4 Bytes
+    
+    suma_flotantes_simpleP:
+        movss (%rsi),%xmm0
+        movss (%rdi),%xmm1
+        addss %xmm1,%xmm0
+        movss %xmm0,(%rdi)
+
+        addq $4,%rdi
+        addq $4,%rsi
+    loop suma_flotantes_simpleP
+
+    ret 
+
+
+sum_simd:
+
+    
+    movq %rdx,%rax
+    xorq %rdx,%rdx
+    
+    movq $4,%r8
+    div %r8
+
+    movq %rax,%rcx
+    
+    suma_flotantes_simpleP_simd:
+        movaps (%rsi),%xmm0
+        movaps (%rdi),%xmm1
+        addps %xmm1,%xmm0
+        movaps %xmm0,(%rdi)
+
+        addq $16,%rdi
+        addq $16,%rsi
+    loop suma_flotantes_simpleP_simd
+    
+    andq %rdx,%rdx
+    jz finalizar
+
+    # 1 <= rdx <= 3
+    # %rdi nos queda el puntero a la posicion del float en a que todavia no sumamos (rax*16)
+    # %rsi nos queda el puntero a la posicion del float en b que todavia no sumamos (rax*16)
+
+    callq sum
+    
+    finalizar:
+    ret
+
+
+    
+
+
+
+
+
+
+
+
+
+
---
+++ Ejercicio 7 y 8/suma.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/resource.h>
+#include <math.h>
+
+float a[]={1.3,2.5,3.5,4.1,0.1};
+float b[]={1.3,2.0,3.5,4.0,0.34};     
+int len=5;
+
+
+void sum(float* a, float* b,int len);
+
+void sum_simd(float* a, float* b,int len);
+
+int main(){
+    
+    struct timespec start,finish;
+    int i;
+
+    
+
+    printf("El resultado de sumar los flotantes:\n\n  a={");
+
+    for(i=0;i<(len-1);i++){
+         printf("%.2f,",a[i]);
+    }
+    printf("%.2f",a[(len-1)]);
+
+    printf("}\n+\n  b={");
+
+    for(i=0;i<(len-1);i++){
+         printf("%.2f,",b[i]);
+    }
+    printf("%.2f",b[(len-1)]);
+
+    printf("}\n------------------------------------\n  a={");
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    sum(a,b,len);
+    clock_gettime(CLOCK_REALTIME, &finish);
+
+    for(i=0;i<(len-1);i++){
+
+         printf("%.2f,",a[i]);
+    }
+    long double  accum = ( finish.tv_sec - start.tv_sec )
+                + ( finish.tv_nsec - start.tv_nsec ) * pow(10,-9);
+    printf("%.2f",a[(len-1)]);
+    printf("}\n\n");
+    printf("El proceso tardo %Lf segs\n",accum);
+
+    
+   
+    return 0;
+}
+
+
+    /*
+   
+    */
