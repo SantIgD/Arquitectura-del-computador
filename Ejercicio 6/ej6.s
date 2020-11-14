648bcdd5a61c34eac203c333b40442c9f085a75c
Santi D <demagistris.santiago@gmail.com>
2020-11-14 10:39:08 -0300

    [Ejercicio 6] Esqueleto de resolucion, falta completarlo y hacerlo andar

--- Ejercicio 6/ej6.s
+++ Ejercicio 6/ej6.s
@@ -11,7 +11,7 @@ filtrar_ceros:
 
 main:
 
-
+ # Suponemos que los coeficientes y el vector de salida tienen componentes distintas a NaN o +- inf
     #rdi x
     #rsi y
 
@@ -22,37 +22,56 @@ main:
     #xmm4 e
     #xmm5 f
 
-    #movss NaN,%xmm0
-    #ucomiss %xmm0,%xmm0
-
-    xorq %rax,%rax
-    incq %rax
-    cvtsi2ssq %rax,%xmm0 
-
-    xorps %xmm0,%xmm0
-
-    divss %xmm0,%xmm0
-
-    xorps %xmm1,%xmm1
-
-    xorq %rax,%rax
-    incq %rax
-    cvtsi2ssq %rax,%xmm1 
-
-    addss %xmm0,%xmm1
-
-
-    
-
-    # x = (c - by)/a
-    # (d/a) (c - by) + ey = f
-    # (d/a) (-by) + ey = f - (d/a) c 
-    # y ((d/a) (-b) + e) = f - (d/a) c 
-    # y = (f - (d/a) c ) / ((d/a) (-b) + e)
-
-
-    
+verificar_coeficientes:
+    # Hacer los cmp necesarios para salvar a esta compania (ver en que caso estamos y saltar al caso)
 
 
 
+case_1: # todos los coeficientes distintos de 0
+
+        #y = (f-(cd)/a) 1/(e-(bd)/a)
+        #x = (c-by)1/a
+        jmp verificar_respuestas
+
+case_2: # a=0, b!=0, d!=0, e!=0
+
+        # y = c/b
+        # x = (f - (ec)/b) 1/d
+        jmp verificar_respuestas
+
+case_3: # a!=0, b=0, d!=0, e!=0
+
+        # x = c/a
+        # y = (f - (dc)/a) 1/e
+        jmp verificar_respuestas
+
+case_4: # a!=0, b!=0, d=0, e!=0
+
+        # y = f/e
+        # x = (c - (bf)/e) 1/a
+        jmp verificar_respuestas
+
+case_5: # a!=0, b!=0, d!=0, e=0
+
+        # x = f/d
+        # y = (c - (af)/d) 1/b
+        jmp verificar_respuestas
+
+case_6: # a=0, b!=0, d!=0, e=0
+
+        # y = c/b
+        # x = f/d
+        jmp verificar_respuestas
+
+case_7: # a!=0, b=0, d=0, e!=0
+
+        # y = f/e
+        # x = c/a
+
+verificar_respuestas:
+    # Verificar si en x o en y tenemos NaN o +-inf
+
+
+
+fin:
     ret
